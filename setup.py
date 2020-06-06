4d6756c759301e9f98d5db934ec9f3f74520f39e
mweath <mweath@uw.edu>
2020-06-05 20:28:58 -0700

    setup.py init

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="KingCountyRealEstate",
+    version="1.0.0",
+    author="Corey Christopherson, Richard Todd, Maggie Weatherly, Ruian Yang",
+    author_email="coreyc2@uw.edu, ,mweath@uw.edu, ruiany@uw.edu",
+    description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/chrico7/data515_project",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
