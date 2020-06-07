99e2f76059aa29dc4a4d6d7935ee92b3cd9abfbc
chrico7 <50036711+chrico7@users.noreply.github.com>
2020-06-07 10:55:02 -0700

    Fixed errors in test_get_county_data.py

--- data515_project/tests/test_get_county_data.py
+++ data515_project/tests/test_get_county_data.py
@@ -8,10 +8,10 @@ class UnitTests(unittest.TestCase):
     def setUp(self):
         """Defines the data frames and column lists to use for testing."""
 
-        self.df_sale = get_county_data("Real%20Property%20Sales")
-        self.df_building = get_county_data("Residential%20Building")
-        self.df_parcel = get_county_data("Parcel")
-        
+        self.df_sale = kc.get_county_data("Real%20Property%20Sales")
+        self.df_building = kc.get_county_data("Residential%20Building")
+        self.df_parcel = kc.get_county_data("Parcel")
+
         self.check_cols_sale = ['Major', 'Minor',
                                 'DocumentDate', 'PropertyType']
         self.check_cols_building = ['Major', 'Minor',
@@ -20,7 +20,7 @@ class UnitTests(unittest.TestCase):
         self.check_cols_parcel = ['Major', 'Minor',
                                   'PropType']
 
-     def test_cols(self):
+    def test_cols(self):
         """Asserts True if dataframe columns match list check_cols."""
 
         sale_col_bool = all(elem in self.df_sale.columns.to_list() for
@@ -29,8 +29,10 @@ class UnitTests(unittest.TestCase):
                                 elem in self.check_cols_building)
         parcel_col_bool = all(elem in self.df_parcel.columns.to_list() for
                               elem in self.check_cols_parcel)
-        
-        self.assertTrue(sale_col_bool and building_col_bool and parce_col_bool)
+
+        self.assertTrue(sale_col_bool and
+                        building_col_bool and
+                        parcel_col_bool)
 
 
     def test_row(self):
@@ -39,7 +41,7 @@ class UnitTests(unittest.TestCase):
         sale_row_bool = self.df_sale.iloc[:1, :].shape[0] >= 1
         building_row_bool = self.df_building.iloc[:1, :].shape[0] >= 1
         parcel_row_bool = self.df_parcel.iloc[:1, :].shape[0] >= 1
-        
+
         self.assertTrue(sale_row_bool and
                         building_row_bool and
                         parcel_row_bool)
