526ddebec352a3101c63ee68bac5fd29d44982ab
mweath <mweath@uw.edu>
2020-06-07 13:06:12 -0700

    uncommented out corey's test

--- data515_project/tests/test_get_county_data.py
+++ data515_project/tests/test_get_county_data.py
@@ -1,5 +1,5 @@
-import os
-import sys
+#import os
+#import sys
 import unittest
 import data515_project.kc_real_estate as kc
 
@@ -22,31 +22,31 @@ class UnitTests(unittest.TestCase):
         self.check_cols_parcel = ['Major', 'Minor',
                                   'PropType']
 
-#    def test_cols(self):
-#        """Asserts True if dataframe columns match list check_cols."""
-#
-#        sale_col_bool = all(elem in self.df_sale.columns.to_list() for
-#                            elem in self.check_cols_sale)
-#        building_col_bool = all(elem in self.df_building.columns.to_list() for
-#                                elem in self.check_cols_building)
-#        parcel_col_bool = all(elem in self.df_parcel.columns.to_list() for
-#                              elem in self.check_cols_parcel)
-#
-#        self.assertTrue(sale_col_bool and
-#                        building_col_bool and
-#                        parcel_col_bool)
+    def test_cols(self):
+        """Asserts True if dataframe columns match list check_cols."""
+
+        sale_col_bool = all(elem in self.df_sale.columns.to_list() for
+                            elem in self.check_cols_sale)
+        building_col_bool = all(elem in self.df_building.columns.to_list() for
+                                elem in self.check_cols_building)
+        parcel_col_bool = all(elem in self.df_parcel.columns.to_list() for
+                              elem in self.check_cols_parcel)
+
+        self.assertTrue(sale_col_bool and
+                        building_col_bool and
+                        parcel_col_bool)
 
 
-#    def test_row(self):
-#        """Asserts True if there are one or more rows in each dataframe."""
-#
-#        sale_row_bool = self.df_sale.iloc[:1, :].shape[0] >= 1
-#        building_row_bool = self.df_building.iloc[:1, :].shape[0] >= 1
-#        parcel_row_bool = self.df_parcel.iloc[:1, :].shape[0] >= 1
-#
-#        self.assertTrue(sale_row_bool and
-#                        building_row_bool and
-#                        parcel_row_bool)
+    def test_row(self):
+        """Asserts True if there are one or more rows in each dataframe."""
+
+        sale_row_bool = self.df_sale.iloc[:1, :].shape[0] >= 1
+        building_row_bool = self.df_building.iloc[:1, :].shape[0] >= 1
+        parcel_row_bool = self.df_parcel.iloc[:1, :].shape[0] >= 1
+
+        self.assertTrue(sale_row_bool and
+                        building_row_bool and
+                        parcel_row_bool)
 
 if __name__ == '__main__':
     unittest.main()
