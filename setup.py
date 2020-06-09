d68200b5d0961bd53ba68b5a6b4f94820da88b8f
Ruian Yang <ruiany1522@gmail.com>
2020-06-09 11:48:36 -0700

    add dependency requirements to setup

--- setup.py
+++ setup.py
@@ -8,11 +8,21 @@ setuptools.setup(
     description="",
     long_description_content_type="text/markdown",
     url="https://github.com/chrico7/data515_project",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(where='./data515_project', exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests',
+        'geopandas',
+        'ipywidgets',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'plotly',
+        'jupyter'
     ],
     python_requires='>=3.6',
 )
